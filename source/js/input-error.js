6ae72996a8325e018865abb7771143056789737e
Ilya Tokovchuk <i.tokovchuk@gmail.com>
2020-05-12 00:05:21 +0300

    доработки

--- source/js/input-error.js
+++ source/js/input-error.js
@@ -1,13 +1,19 @@
-var form = document.querySelector(".form");
-var name = form.querySelector(".cat-name");
-var weight = form.querySelector(".cat-weight");
-var tel = form.querySelector(".form__input--tel");
-var email = form.querySelector(".form__input--email");
+var form = document.querySelector('.form');
+var catname = document.querySelector('.cat-name');
+var weight = document.querySelector('.cat-weight');
+var tel = document.querySelector('.form__input--tel');
+var email = document.querySelector('.form__input--email');
 
-form.addEventListener("submit", function(evt){
+console.log(catname);
+form.addEventListener('submit', function(evt){
   evt.preventDefault();
-	if (!name.value) {
-		name.classList.add("form__input--error");
-	}
+	if (!catname.value) {
+		catname.classList.add('form__input--error');
+	} else if (!weight.value) {
+    weight.classList.add('form__input--error');
+  } else if (!email.value) {
+    email.classList.add('form__input--error');
+  } else if (!tel.value) {
+    tel.classList.add('form__input--error');
+  } else {form.submit();}
 });
-
