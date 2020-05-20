3e011d93059b2543330d6ae5cd3802f598424bb0
Ilya Tokovchuk <i.tokovchuk@gmail.com>
2020-05-21 00:23:20 +0300

    fix ie

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ gulp.task ("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
     "source/js/**",
     "source/*.ico"
     ], {
@@ -99,5 +100,5 @@ gulp.task("refresh", function(done) {
 });
 
 
-gulp.task("build", gulp.series("clean", "copy", "images", "webp", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "webp", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
