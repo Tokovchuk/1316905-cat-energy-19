d0edf55f78e990ecf4e825f3157beb990c4f7450
Ilya Tokovchuk <i.tokovchuk@gmail.com>
2020-05-20 22:05:53 +0300

    menu--nojs + icon-htmlacademy + pp

--- source/js/input-error.js
+++ source/js/input-error.js
@@ -1,33 +1,33 @@
-var form = document.querySelector('.form');
-var catname = document.querySelector('.cat-name');
-var weight = document.querySelector('.cat-weight');
-var tel = document.querySelector('.form__input--tel');
-var email = document.querySelector('.form__input--email');
-var button = document.querySelector('.form__button');
+var form = document.querySelector(".form");
+var catname = document.querySelector(".cat-name");
+var weight = document.querySelector(".cat-weight");
+var tel = document.querySelector(".form__input--tel");
+var email = document.querySelector(".form__input--email");
+var button = document.querySelector(".form__button");
 
 
-button.addEventListener('click', function(evt){
+button.addEventListener("click", function(evt){
   var inputError=false
   evt.preventDefault();
-  catname.classList.remove('form__input--error');
-  weight.classList.remove('form__input--error');
-  email.classList.remove('form__input--error');
-  tel.classList.remove('form__input--error');
+  catname.classList.remove("form__input--error");
+  weight.classList.remove("form__input--error");
+  email.classList.remove("form__input--error");
+  tel.classList.remove("form__input--error");
 
   if (!catname.value) {
-    catname.classList.add('form__input--error');
+    catname.classList.add("form__input--error");
     inputError=true;
   }
   if (!weight.value) {
-    weight.classList.add('form__input--error');
+    weight.classList.add("form__input--error");
     inputError=true;
   }
   if (!email.value) {
-    email.classList.add('form__input--error');
+    email.classList.add("form__input--error");
     inputError=true;
   }
   if (!tel.value) {
-    tel.classList.add('form__input--error');
+    tel.classList.add("form__input--error");
     inputError=true;
   }
   if (!inputError) {
